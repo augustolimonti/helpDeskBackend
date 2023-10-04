9d38d3bde8541e3f340d8601107eec7304e93383
Augusto Limonti <agulimonti09@gmail.com>
2023-10-04 09:11:05 +0200

    update

--- helpdeskbackend/settings.py
+++ helpdeskbackend/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
